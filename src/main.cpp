5ce1c732f6b606b1264c7f690c9334357ef12568
AverageUser125 <96657216+AverageUser125@users.noreply.github.com>
2024-09-26 15:06:27 +0300

    allow console support

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include "coreDllinj.hpp"
 #include "gui.hpp"
+#include <cstdio>
 
+// #define NO_CONSOLE
 
 int main() {
 	EnableDebugPrivilege();
 	#ifndef NO_CONSOLE
+	AllocConsole();
+	freopen("conin$", "r", stdin);
+	freopen("conout$", "w", stdout);
+	freopen("conout$", "w", stderr);
+	std::cout.sync_with_stdio();
+
 	// Enable output of Unicode in the console
 	SetConsoleOutputCP(CP_UTF8);
 	std::wcout.imbue(std::locale("en_US.UTF-8"));
